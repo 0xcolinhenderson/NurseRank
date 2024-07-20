a791c50b2526425608606a3dae71a9b3948b400c
0xcolinhenderson <153252937+0xcolinhenderson@users.noreply.github.com>
2024-07-20 22:47:58 +0000

    a

--- app/views/static_views.py
+++ app/views/static_views.py
@@ -2,50 +2,34 @@ from flask import render_template
 from flask_login import login_required
 from ..permissions import roles_required
 
-index_navbar = {
+navbar = {
     'Home' : '/',
-    'Log In' : '/login',
-    'Register' : 'register',
     'About' : '/about',
+}
+
+admin_navbar = {
+    'Home' : '/',
+    'About' : '/about',
+    'Manage' : '/admin'
 }
 
 def index():
-    return render_template("index.html", current_page='home', navbar_pages = index_navbar)
-
-register_navbar = {
-    'Home' : '/',
-    'About' : '/about',
-}
+    return render_template("index.html", current_page='home', navbar_pages = navbar)
 
 def register():
-    return render_template("register.html",current_page='register', navbar_pages = register_navbar)
-
-login_navbar = {
-    'Home' : '/',
-    'About' : '/about',
-}
+    return render_template("register.html",current_page='register', navbar_pages = navbar)
 
 def login():
-    return render_template("login.html",current_page='login')
-
-about_navbar = {
-    'Home' : '/',
-    'Log In' : '/login',
-    'Register' : 'register',
-    'About' : '/about',
-}
+    return render_template("login.html",current_page='login', navbar_pages = navbar)
 
 def about():
-    return render_template("about.html",current_page='about')
-
-
+    return render_template("about.html",current_page='about', navbar_pages = navbar)
 
 @login_required
 def account():
-    return render_template("account.html",current_page='account')
-
+    return render_template("account.html",current_page='account', navbar_pages = navbar)
 
 @login_required
 @roles_required(["admin"])
 def admin():
-    return render_template("admin.html",current_page='admin')
+    return render_template("admin.html",current_page='admin', navbar_pages = admin_navbar)
